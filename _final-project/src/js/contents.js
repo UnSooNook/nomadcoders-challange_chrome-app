91ea4750d021404f045d4f3c18fa92fc0b5ed27d
blackbeaver37 <fhkaldh@naver.com>
2020-08-16 22:10:10 +0900

    version 1.0

--- _final-project/src/js/contents.js
+++ _final-project/src/js/contents.js
@@ -4,19 +4,35 @@
 //     weatherBtn = document.querySelector(".menu-block__weather");
 
 const clockContent = document.querySelector(".contents-clock"),
-    todoContent = document.querySelector(".contents-todo");
+    todoContent = document.querySelector(".contents-todo"),
+    weatherContent = document.querySelector(".contents-weather");
 
-const contentList = [clockContent, todoContent];
+const contentList = [clockContent, todoContent, weatherContent];
 
-function selectContents(event) {
-    console.log(event);
+const FADE_IN = "fadein-point",
+    FADE_OUT = "fadeout-point";
+
+let previousContent;
+
+function changeContent() {
+    const contentID_string = localStorage.getItem(MENU_LS);
+    const contentID = parseInt(contentID_string, 10);
+
+    showOffAll();
+    contentList[contentID].classList.remove(SHOWING_OFF);
+    contentList[contentID].classList.add(FADE_IN);
 }
 
 function showOffAll() {
     clockContent.classList.remove(SHOWING_OFF);
+    clockContent.classList.remove(FADE_IN);
     todoContent.classList.remove(SHOWING_OFF);
+    todoContent.classList.remove(FADE_IN);
+    weatherContent.classList.remove(SHOWING_OFF);
+    weatherContent.classList.remove(FADE_IN);
     clockContent.classList.add(SHOWING_OFF);
     todoContent.classList.add(SHOWING_OFF);
+    weatherContent.classList.add(SHOWING_OFF);
 }
 
 function loadContent() {
@@ -25,18 +41,15 @@ function loadContent() {
         loadedContent = DEFAULT_MENU;
     }
     showOffAll();
-    contentList[parseInt(loadedContent)].classList.remove(SHOWING_OFF);
+    contentList[parseInt(loadedContent, 10)].classList.remove(SHOWING_OFF);
+    previousContent = parseInt(loadedContent, 10);
 }
 
 function init() {
     loadContent();
-    window.addEventListener(
-        "storage",
-        function (evt) {
-            console.log(evt);
-        },
-        false
-    );
+    clockBtn.addEventListener("click", changeContent);
+    todoBtn.addEventListener("click", changeContent);
+    weatherBtn.addEventListener("click", changeContent);
 }
 
 init();
